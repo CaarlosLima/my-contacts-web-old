94d4ac4d833984a2dc886248aebc01b43af686ec
Carlos Eduardo de Lima <caduu@live.com>
2023-04-28 11:18:37 -0300

    Criado evento de `Toast`

--- src/components/Toast/ToastContainer/index.js
+++ src/components/Toast/ToastContainer/index.js
@@ -1,12 +1,29 @@
+import { useEffect, useState } from 'react';
 import ToastMessage from '../ToastMessage';
 import { Container } from './styles';
 
 export default function ToastContainer() {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    function handleAddToast(event) {
+      const { type, text } = event.detail;
+
+      setMessages((prevMessages) => [...prevMessages, { id: Math.random(), type, text }]);
+    }
+
+    document.addEventListener('addtoast', handleAddToast);
+
+    return () => {
+      document.removeEventListener('addtoast', handleAddToast);
+    };
+  }, []);
+
   return (
     <Container>
-      <ToastMessage text="Default Toast" />
-      <ToastMessage text="Error Toast" type="danger" />
-      <ToastMessage text="Success Toast" type="success" />
+      {messages.map((message) => (
+        <ToastMessage key={message.id} text={message.text} type={message.type} />
+      ))}
     </Container>
   );
 }
--- src/pages/NewContact/index.js
+++ src/pages/NewContact/index.js
@@ -1,6 +1,7 @@
 import ContactForm from 'components/ContactForm';
 import PageHeader from 'components/PageHeader';
 import ContactsService from 'services/ContactsService';
+import toast from 'utils/toast';
 
 export default function NewContact() {
   async function handleSubmit(formData) {
@@ -12,11 +13,11 @@ export default function NewContact() {
         category_id: formData.categoryId,
       };
 
-      const response = await ContactsService.createContacts(contact);
+      await ContactsService.createContacts(contact);
 
-      console.log(response);
+      toast({ type: 'success', text: 'Contato cadastrado com sucesso!' });
     } catch (error) {
-      alert('Error creating contact', error);
+      toast({ type: 'danger', text: error.message });
     }
   }
 
