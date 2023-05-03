0eecd1b556c3552e3c82a74da24a69536873ca21
Carlos Eduardo de Lima <caduu@live.com>
2023-05-03 10:34:32 -0300

    Adicionado reset do formulário

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -71,6 +71,12 @@ const ContactForm = forwardRef(({ buttonLabel, onSubmit }, ref) => {
       setPhone(formatPhone(contact.phone ?? ''));
       setCategoryId(contact.category_id ?? '');
     },
+    resetFields: () => {
+      setName('');
+      setEmail('');
+      setPhone(formatPhone(''));
+      setCategoryId('');
+    },
   }), []);
 
   useEffect(() => {
--- src/pages/NewContact/index.js
+++ src/pages/NewContact/index.js
@@ -1,9 +1,11 @@
 import ContactForm from 'components/ContactForm';
 import PageHeader from 'components/PageHeader';
+import { useRef } from 'react';
 import ContactsService from 'services/ContactsService';
 import toast from 'utils/toast';
 
 export default function NewContact() {
+  const contactFormRef = useRef(null);
   async function handleSubmit(formData) {
     try {
       const contact = {
@@ -15,6 +17,8 @@ export default function NewContact() {
 
       await ContactsService.createContact(contact);
 
+      contactFormRef.current.resetFields();
+
       toast({ type: 'success', text: 'Contato cadastrado com sucesso!' });
     } catch (error) {
       toast({ type: 'danger', text: error.message });
@@ -24,7 +28,7 @@ export default function NewContact() {
   return (
     <>
       <PageHeader title="Novo Contato" />
-      <ContactForm buttonLabel="Cadastrar" onSubmit={handleSubmit} />
+      <ContactForm ref={contactFormRef} buttonLabel="Cadastrar" onSubmit={handleSubmit} />
     </>
   );
 }
